9cc1c0ad062b83b4db3113d9c66bd28846ab9be2
SamTi（三台） <a44281071@sina.com>
2024-10-30 14:43:10 +0800

    update for .net8 to sucess build.

--- src/StdfSharpTests/Constants.cs
+++ src/StdfSharpTests/Constants.cs
@@ -5,16 +5,17 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>
  */
 
+using System;
 using System.IO;
 using NUnit.Framework;
 
@@ -22,9 +23,14 @@ namespace KA.StdfSharp.Tests
 {
     class Constants
     {
-        public static readonly string TestStdfFilePath = Path.GetFullPath(@"../../../../../../../stdf/stdf_test.std");
-        public static readonly string TemporaryTestStdfFilePath = Path.GetFullPath(@"../../../../../../../stdf/tmp.std");
-        
+        static Constants()
+        {
+            var stdfDirectory = Directory.CreateDirectory(Path.Combine(Environment.GetEnvironmentVariable("TEMP"), "stdf"));
+            TestStdfFilePath = Path.Combine(stdfDirectory.FullName, "stdf_test.std");
+            TemporaryTestStdfFilePath = Path.Combine(stdfDirectory.FullName, "tmp.std");
+        }
+
+        public static readonly string TestStdfFilePath;
+        public static readonly string TemporaryTestStdfFilePath;
     }
-    
 }
